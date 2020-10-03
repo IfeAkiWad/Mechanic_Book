fedc28ac28961ec96e1106270866721fa8e4b18b
timothyphilip <philippetimothe@gmail.com>
2020-10-03 16:55:53 -0400

    CRUD functions implementations

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,2 +1,26 @@
 class VehiclesController < ApplicationController
+
+    def index
+        @vehicles = Vehicle.all
+    end
+
+    def show
+        @vehicle = Vehicle.find_by_id(params[:id])
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
 end
--- db/migrate/20201001173253_create_vehicles.rb
+++ db/migrate/20201001173253_create_vehicles.rb
@@ -1,9 +1,13 @@
 class CreateVehicles < ActiveRecord::Migration[6.0]
   def change
     create_table :vehicles do |t|
+      t.string :owner_name
+      t.integer :vin_number
       t.string :make
       t.string :model
+      t.integer :year
       t.string :type
+      t.integer :milage
       t.string :symptom
 
       t.timestamps
