6f2f091773ad6953136f66ed928844b06b130bc9
timothyphilip <philippetimothe@gmail.com>
2020-10-06 11:44:04 -0400

    Definition of attributes method in repairs model

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -2,4 +2,10 @@ class Repair < ApplicationRecord
     belongs_to :mechanic
     belongs_to :vehicle
     accepts_nested_attributes_for :vehicle 
+
+    def vehicle_attributes=(vehicle_attributes)
+        vehicle = Vehicle.find_or_create_by(vehicle_attributes)
+        self.vehicle = vehicle
+        save 
+    end
 end
