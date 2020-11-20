ddc1b7894ce7e07d8492fb11a1a5eb80a4f7a091
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-11-20 00:58:07 -0600

    root 'mechanics#index'

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback', to: 'sessions#omniauth'
 
+  root 'mechanics#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
